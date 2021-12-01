925e4f29e75ff26b05a56b28397d8b798f1af115
Fabio Marturano <fabiocmjor@gmail.com>
2021-12-01 13:15:29 -0300

    rename file initial_commands-json files

---
+++ py/commands/json.py
@@ -0,0 +1,23 @@
+import json
+
+# Leitura de todos os pokemons
+with open("pokemons.json") as file:
+    pokemons = json.load(file)["results"]
+
+# Separamos somente os do tipo grama
+grass_type_pokemons = [
+    pokemon for pokemon in pokemons if "Grass" in pokemon["type"]
+]
+
+# Abre o arquivo para escrevermos apenas o pokemons do tipo grama
+with open("grass_pokemons.json", "w") as file:
+    json_to_write = json.dumps(
+        grass_type_pokemons
+    )  # conversão de Python para o formato json (str)
+    file.write(json_to_write)
+
+
+# abre o arquivo para escrita
+# with open("grass_pokemons.json", "w") as file:
+#     # escreve no arquivo já transformando em formato json a estrutura
+#     json.dump(grass_type_pokemons, file)
