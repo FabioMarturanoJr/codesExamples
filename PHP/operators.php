cf9dae09f3e0a8c57e067d4df9a1dd110c618c21
Fabio Marturano <fabiocmjor@gmail.com>
2021-12-16 14:17:50 -0300

    operators

---
+++ PHP/operators.php
@@ -0,0 +1,10 @@
+<?php
+
+$currentAge = 27;
+
+echo ($currentAge + 10) . "\n";
+echo ($currentAge - 10) . "\n";
+echo ($currentAge * 10) . "\n";
+echo ($currentAge / 10) . "\n";
+echo ($currentAge % 10) . "\n";
+echo ($currentAge ** 2) . "\n";
