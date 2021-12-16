3bede540cbfff16745e250affedbcfe8c8c1676b
Fabio Marturano <fabiocmjor@gmail.com>
2021-12-16 15:34:57 -0300

    primitive types

---
+++ PHP/dataTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+$age = 21;
+$salary = 2000.5;
+$name = 'fabio';
+$binary = true;
+
+echo 'Data Types' . "\n";
+echo gettype($age) . "\n";
+echo gettype($salary) . "\n";
+echo gettype($name) . "\n";
+echo gettype($binary) . "\n";
