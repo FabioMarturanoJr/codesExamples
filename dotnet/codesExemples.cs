fd510325c7833a3793bf36ae7bfbfb292939613e
Fabio Marturano <fabiocmjor@gmail.com>
2021-12-28 15:14:29 -0300

    remove unnecessary files

--- dotnet/codesExemples.cs
+++ dotnet/codesExemples.cs
@@ -15,17 +15,36 @@ class Program {
 
         bool ofCorse = true;
 
+        int month = DateTime.Now.Month;
+
         // cast to other dataType
         age = (int)num;
+
+
+        System.Console.WriteLine("Write your name");
+        System.Console.WriteLine("Hello " + Console.ReadLine() + ":)");
 
 
         Console.WriteLine($"My age {age}, my salary {salary - num}");
         Console.WriteLine($"My Height {height}");
         Console.WriteLine($"Age of universe {universeAge}, total habitable planets {habitablePlanets}");
-        Console.WriteLine($"ASC 65 {letter}, ASC 66 {(char)(letter + 1)}");
+        Console.WriteLine($"ASC 65 {letter}, ASC 66 {(char)(letter++)}");
         Console.WriteLine($"Some Stacks\n{stacks}");
         Console.WriteLine(age >= 18 ? "Can Enter" : "Sorry Maybe later");
         if (ofCorse) Console.WriteLine("I'm a very nice Programer !!!");
+        Console.WriteLine(month);
+
+        switch (month)
+        {
+            case 12:
+                System.Console.WriteLine("Happy Birthday Fabio !!!");
+                break;
+            
+            default:
+                System.Console.WriteLine("Not Yet :(");
+                break;
+        }
+
         // Console.ReadLine();
     }
 }
