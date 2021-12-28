ff1fbe927c840007b71dbe6a2b8318d3ea8e179b
Fabio Marturano <fabiocmjor@gmail.com>
2021-12-28 14:27:24 -0300

    more exemples cSharp

--- dotnet/consoleTest/Program.cs
+++ dotnet/consoleTest/Program.cs
@@ -10,10 +10,22 @@ class Program {
         long universeAge = 130000000000000;
         short habitablePlanets = 1;
 
+        char letter = (char)65;
+        string stacks = "- .NET \n- JavaScript";
+
+        bool ofCorse = true;
+
         // cast to other dataType
         age = (int)num;
 
-        Console.WriteLine($"My age {age}, my salary {salary + num}");
+
+        Console.WriteLine($"My age {age}, my salary {salary - num}");
+        Console.WriteLine($"My Height {height}");
+        Console.WriteLine($"Age of universe {universeAge}, total habitable planets {habitablePlanets}");
+        Console.WriteLine($"ASC 65 {letter}, ASC 66 {(char)(letter + 1)}");
+        Console.WriteLine($"Some Stacks\n{stacks}");
+        Console.WriteLine(age >= 18 ? "Can Enter" : "Sorry Maybe later");
+        if (ofCorse) Console.WriteLine("I'm a very nice Programer !!!");
         // Console.ReadLine();
     }
 }
