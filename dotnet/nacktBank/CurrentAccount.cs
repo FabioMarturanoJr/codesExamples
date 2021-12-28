1cd3109e560f74934cb0d41e9d8531c3b852cbda
Fabio Marturano <fabiocmjor@gmail.com>
2021-12-28 17:36:30 -0300

    add other userAccout && impremente withdraw, deposit, transfer functions

--- dotnet/nacktBank/CurrentAccount.cs
+++ dotnet/nacktBank/CurrentAccount.cs
@@ -1,10 +1,22 @@
 namespace nacktBank
 {
-  public class CurrentAccount
-    {
-    public string owner = "User";
-    public int agencyNumb;
-    public int accountNumb;
-    public double balance;
+    public class CurrentAccount{
+        public string owner = "User";
+        public int agencyNumb;
+        public int accountNumb;
+        public double balance;
+        public bool Withdraw(double value) {
+            if(this.balance == 0 ||this.balance < value) return false;
+            this.balance -= value;
+            return true;
+        }
+        public void Deposit(double value) {
+            this.balance += value;
+        }
+        public bool Transfer(double value, CurrentAccount destAccount) { 
+            if (!this.Withdraw(value)) return false;
+            destAccount.Deposit(value);
+            return true;
+        }
     }
 }
--- dotnet/nacktBank/Program.cs
+++ dotnet/nacktBank/Program.cs
@@ -5,13 +5,23 @@
         public static void Main(string[] args)
         {
             CurrentAccount fabioAccount = new CurrentAccount();
+            CurrentAccount brunaAccount = new CurrentAccount();
 
             fabioAccount.owner = "fabio";
             fabioAccount.agencyNumb = 4242;
             fabioAccount.accountNumb = 098765;
-            fabioAccount.balance = 6500;
+            fabioAccount.balance = 300;
 
-            System.Console.WriteLine(fabioAccount);
+            brunaAccount.owner = "bruna";
+            brunaAccount.agencyNumb = 4242;
+            brunaAccount.accountNumb = 198765;
+            brunaAccount.balance = 4300;
+
+            System.Console.WriteLine(fabioAccount.balance);
+            System.Console.WriteLine(brunaAccount.balance);
+            System.Console.WriteLine(fabioAccount.Transfer(500,brunaAccount));
+            System.Console.WriteLine(fabioAccount.balance);
+            System.Console.WriteLine(brunaAccount.balance);
         }
     }
 }
