d8207b3b59bdd847d315a20f912881f4e176797f
Fabio Marturano <fabiocmjor@gmail.com>
2022-01-05 17:44:27 -0300

    comment the genetate xml, defaut some employees password

--- dotnet/NacktBank/NacktBank.Models/Employees/EmployeeAuthenticable.cs
+++ dotnet/NacktBank/NacktBank.Models/Employees/EmployeeAuthenticable.cs
@@ -6,7 +6,7 @@ namespace NacktBank.Models.Employees
     public abstract class EmployeeAuthenticable : Employee, IAuthenticable
     {
         private AuthenticateHelper _auth = new AuthenticateHelper();
-        public string? Password { get; set; }
+        public string Password { get; set; } = "undefined";
         protected EmployeeAuthenticable(double salary, string cpf) : base(salary, cpf)
         {
         }
--- dotnet/NacktBank/NacktBank.Models/Partners/CommercialPartiner.cs
+++ dotnet/NacktBank/NacktBank.Models/Partners/CommercialPartiner.cs
@@ -6,7 +6,7 @@ namespace NacktBank.Models.Partners
     public class CommercialPartiner : IAuthenticable
     {
         private AuthenticateHelper _auth = new AuthenticateHelper();
-        public string? Password { get; set; }
+        public string Password { get; set; } = "undefined";
         public string? Name { get; set; }
         public bool Authenticate(string password)
         {
