75b093e711ff1a2761940c5d2df029c795822640
Fabio Marturano <fabiocmjor@gmail.com>
2022-01-11 14:35:03 -0300

    using override and arrayInt

--- dotnet/NacktBank/NacktBank.AgencySystem/Program.cs
+++ dotnet/NacktBank/NacktBank.AgencySystem/Program.cs
@@ -1,16 +1,1 @@
-﻿using NacktBank.Models.CurrentAccounts;
-
-CurrentAccount fabioAcc = new CurrentAccount(123,4242);
-Client fabio = new Client();
-fabio.Name = "Fabio Marturano";
-fabio.CPF = "123.123.123-5";
-fabio.Occupation = "Dev";
-
-Client fabio2 = new Client();
-fabio2.Name = "Fabio Marturano";
-fabio2.CPF = "123.123.123-5";
-fabio2.Occupation = "Dev";
-
-System.Console.WriteLine(fabioAcc);
-System.Console.WriteLine(fabio.Equals(fabio2));
-System.Console.WriteLine(fabio.Equals(fabioAcc));
+﻿
---
+++ dotnet/NacktBank/NacktBank.AgencySystem/tests.helper/ArrayInt.cs
@@ -0,0 +1,20 @@
+// int[] ages = new int[4];
+
+// ages[0] = 5;
+// ages[1] = 15;
+// ages[2] = 25;
+// ages[3] = 35;
+
+// int index = 0;
+// int resultWithWhile = 0;
+// while (index < ages.Length)
+// {
+//     resultWithWhile += ages[index];
+//     index++;
+// }
+
+// var resultWithReducer = ages.Aggregate((acc, curr) => acc + curr);
+
+// System.Console.WriteLine(resultWithWhile);
+// System.Console.WriteLine(resultWithReducer);
+// System.Console.WriteLine(ages[3]);
