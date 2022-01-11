1d3d3591cd53505190e8ec69443c5105fc4175ad
Fabio Marturano <fabiocmjor@gmail.com>
2022-01-11 08:38:13 -0300

    101 front students

---
+++ dotnet/students-front/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Fabio Marturano</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
